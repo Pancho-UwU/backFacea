f0f7169d8482dd6e70dc6f150e6e1ef758e37385
bxnjadev <benjamin.hyx@gmail.com>
2025-04-10 21:12:38 -0400

    fix: fixed name host cors

--- src/middlewares/corsMiddelwares.js
+++ src/middlewares/corsMiddelwares.js
@@ -1,9 +1,12 @@
 import cors from 'cors'
 export const corsMiddleware= ()=>cors({
     origin:(origin,callback)=>{
+        console.log(origin);
+        console.log(callback);
         const ACCEPTED_ORIGINS = [
             'http://localhost:8080',
-            'http://localhosto:3000'
+            'http://localhost:3000',
+            'http://localhost:4200'
         ]
         if(ACCEPTED_ORIGINS.includes(origin)){
             return callback(null,true)
